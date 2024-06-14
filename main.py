e2511d7698e75ab7a5876a1f75f62ed8ad707c0d
dargen <dargencode@mail.ru>
2024-06-14 14:22:16 +0300

    debug for starting ;d

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from work.bot import run_fetch, run_work
+from work.bot import run_fetch, run_work, clients
 from work.dumper import run_dump
+
+print('Staring BlumBot')
+print(f'Loaded {len(clients)} accounts')
 
 run_fetch()
 run_dump()
